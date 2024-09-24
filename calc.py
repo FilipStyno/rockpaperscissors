e9dfe849c168a73f01ff473f86e001aceb4e656a
Filip Hřivňacký <118018400+FilipStyno@users.noreply.github.com>
2024-09-24 14:36:02 +0200

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 import random
 
 def pc():
-    num = random.randint(1,3)
+    num = random.randint(1, 3)
     if num == 1:
         return "rock"
     elif num == 2:
@@ -9,29 +9,34 @@ def pc():
     elif num == 3:
         return "scissors"
 
-u_input = input("Enter 'rock' 'paper' or 'scissors'")
-
-
 def result():
+    u_input = input("Enter 'rock', 'paper', or 'scissors': ").lower()
     comp = pc()
+    
     if u_input == "rock" and comp == "scissors":
-        print("computer chose scissors u win")
+        print("Computer chose scissors, you win!")
     elif u_input == "rock" and comp == "paper":
-        print("computer chose paper u lose")
+        print("Computer chose paper, you lose.")
     elif u_input == "rock" and comp == "rock":
-        print("computer chose rock its a tie")
+        print("Computer chose rock, it's a tie.")
     elif u_input == "paper" and comp == "scissors":
-        print("computer chose scissors u lose")
+        print("Computer chose scissors, you lose.")
     elif u_input == "paper" and comp == "paper":
-        print("computer chose paper its a tie")
+        print("Computer chose paper, it's a tie.")
     elif u_input == "paper" and comp == "rock":
-        print("computer chose rock u win")
+        print("Computer chose rock, you win!")
     elif u_input == "scissors" and comp == "scissors":
-        print("computer chose scissors its a tie")
+        print("Computer chose scissors, it's a tie.")
     elif u_input == "scissors" and comp == "paper":
-        print("computer chose paper u win")
+        print("Computer chose paper, you win!")
     elif u_input == "scissors" and comp == "rock":
-        print("computer chose rock its u lose")
+        print("Computer chose rock, you lose.")
     else:
-        print("ur retarded")
-result()
+        print("Invalid input.")
+
+while True:
+    result()
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thanks for playing!")
+        break
